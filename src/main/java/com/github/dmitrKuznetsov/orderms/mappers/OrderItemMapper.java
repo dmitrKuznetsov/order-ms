4926c645af8e857ed4a6daec912547fd893bf64f
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2022-06-30 10:32:41 +0300

    Added OrderItemMapper

--- src/main/java/com/github/dmitrKuznetsov/orderms/mappers/OrderItemMapper.java
+++ src/main/java/com/github/dmitrKuznetsov/orderms/mappers/OrderItemMapper.java
@@ -1,13 +1,16 @@
 package com.github.dmitrKuznetsov.orderms.mappers;
 
 import com.github.dmitrKuznetsov.orderms.mappers.entities.Order;
+import com.github.dmitrKuznetsov.orderms.mappers.entities.OrderItem;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 
 @Mapper
-public interface OrderMapper {
-    @Select("SELECT * FROM \"order\" WHERE ID = #{id}")
-    Order findById(@Param("id") int id);
+public interface OrderItemMapper {
+    @Select("SELECT * FROM ORDER_ITEM WHERE ORDER_ID = #{id}")
+    List<OrderItem> findByOrderId(@Param("id") int id);
 }
--- src/main/java/com/github/dmitrKuznetsov/orderms/services/OrderService.java
+++ src/main/java/com/github/dmitrKuznetsov/orderms/services/OrderService.java
@@ -1,19 +1,27 @@
 package com.github.dmitrKuznetsov.orderms.services;
 
+import com.github.dmitrKuznetsov.orderms.mappers.OrderItemMapper;
 import com.github.dmitrKuznetsov.orderms.mappers.OrderMapper;
 import com.github.dmitrKuznetsov.orderms.mappers.entities.Order;
+import com.github.dmitrKuznetsov.orderms.mappers.entities.OrderItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderService {
 
     @Autowired
     public OrderMapper orderMapper;
+    @Autowired
+    public OrderItemMapper orderItemMapper;
 
     public Order findById(int id) {
         Order order = orderMapper.findById(id);
-        System.out.println(order);
-        return orderMapper.findById(id);
+        ArrayList<OrderItem> orderItems = new ArrayList(orderItemMapper.findByOrderId(id));
+        order.setOrders(orderItems);
+        return order;
     }
 }
